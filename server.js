b8c7a77c92d5b2119da490007230365bcd02c7a4
Edward Tanguay (wk) <edward@tanguay.info>
2020-09-01 14:03:33 +0200

    Implement the first ejs page with variables from backend

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
-app.use(express.static(path.join(__dirname, './static')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './public/views'));
 
-app.get('/speakers', (req, res) => {
-	res.sendFile(path.join(__dirname, './static/speakers.html'));
+const staticDirectory = path.join(__dirname, './public');
+app.use(express.static(staticDirectory));
+
+app.get('/', (req, res) => {
+	res.render('pages/index', { pageTitle: 'The Main Page' });
+});
+
+app.get('/info', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/info.html'));
 });
 
 app.listen(port, () => {
