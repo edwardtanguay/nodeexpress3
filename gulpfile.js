6c43a52c88139eb3eac1aa44b2c997848ad6640b
Edward Tanguay (wk) <edward@tanguay.info>
2020-09-02 10:40:44 +0200

    Set up gulp and sass as watch

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+
+gulp.task('styles', () => {
+	return gulp.src('public/sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('clean', () => {
+	return del([
+		'public/css/main.css',
+	]);
+});
+
+gulp.task('watch', () => {
+	gulp.watch('public/sass/**/*.scss', (done) => {
+		gulp.series(['clean', 'styles'])(done);
+	});
+});
+
+//gulp.task('default', gulp.series(['clean', 'styles']));
+gulp.task('default', gulp.series(['watch']));
