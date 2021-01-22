6fc3bd5aef1c30c233c13bd0533afc51798e9a8a
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-01-22 20:37:49 +0100

    404 page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieSession = require('cookie-session');
 const routes = require('./public/routes');
+const httpErrors = require('http-errors');
 
 const config = {
 	'appTitle': 'Node.js/Express Manual Site'
@@ -32,22 +33,43 @@ app.locals.appVersion = "0.23.45";
 app.locals.mainFlashcards = [
 	{
 		"front": "to show",
-		"back": "mostrar" 
+		"back": "mostrar"
 	},
 	{
 		"front": "to win",
-		"back": "ganar" 
+		"back": "ganar"
 	},
 	{
 		"front": "to delete",
-		"back": "borrar" 
+		"back": "borrar"
 	}
 ];
 
 const staticDirectory = path.join(__dirname, './public');
 app.use(express.static(staticDirectory));
 
+app.use('/throw', (req, res, next) => {
+	setTimeout(() => {
+		// throw new Error('something went wrong')
+		return next(new Error('something went wrong'));
+	}, 4000);
+});
+
 app.use('/', routes(config));
+
+app.use((req, res, next) => {
+	return next(httpErrors(404, 'File not found.'));
+})
+
+// 404 page
+app.use((err, req, res, next) => {
+	console.log(err); // log error but don't show to users
+	res.locals.message = err.message;
+	const status = err.status || 500;
+	res.locals.status = status;
+	res.status(status);
+	res.render('error');
+});
 
 app.listen(port, () => {
 	console.log(`app listening at: http://localhost:${port}`);
